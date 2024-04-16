a9a32ce22f3828208c37196b60690cf55009305f
Egoitz De la Llera <egoitz.delallera@ikasle.egibide.org>
2024-04-16 14:05:59 +0200

    Commit de controlador y controlador de inmueble

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,20 +1,81 @@
 package controlador;
 
-import vista.VentanaPrincipal;
+import vista.*;
 
 import javax.naming.ldap.Control;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Controlador {
 
     private ControladorUsuario cu;
     private ControladorInmueble ci;
-
     private VentanaPrincipal vp;
+
 
     public Controlador(){
         // Creacion de Controladores y Ventanas
         cu = new ControladorUsuario(this);
         ci = new ControladorInmueble(this);
         vp = new VentanaPrincipal();
+
+
+
+
+
+    }
+
+    public void usoVentanaPrincipal() {
+        vp.addMiAltaIAl(new alAInmuebles());
+        vp.addMiAltaPAl(new alAPersonas());
+        vp.addMiBajaIAl(new alBInmuebles());
+        vp.addMiBajaPAl(new alBPersonas());
+        vp.addMiModificacionIAl(new alMInmuebles());
+        vp.addMiModificacionPAl(new alMPersonas());
+
+        vp.setVisible(true);
+    }
+
+    public class alAInmuebles implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    public class alAPersonas implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    public class alBInmuebles implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+
+    public class alBPersonas implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    public class alMInmuebles implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    public class alMPersonas implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
--- src/controlador/ControladorInmueble.java
+++ src/controlador/ControladorInmueble.java
@@ -1,12 +1,73 @@
 package controlador;
 
 import modelo.Inmueble;
+import vista.*;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class ControladorInmueble {
     private Controlador cont;
     private Inmueble inm;
+    private VentanaAltaInmuebles vai;
+    private VentanaBajaInmuebles vbi;
+    private VentanaModificacionInmuebles vmi;
+
 
     public ControladorInmueble(Controlador cont){
         this.cont = cont;
     }
-}
+
+    public void altaInmuebles(){
+        vbi = new VentanaBajaInmuebles();
+
+        vai.addAceptarListener(new aceptarAltaMouseListener());
+        vai.addSalirListener(new salirMouseListener());
+
+        vbi.setVisible(true);
+    }
+
+    public void bajaInmuebles(){
+        vmi = new VentanaModificacionInmuebles();
+
+        vai.addAceptarListener(new aceptarMouseListener());
+        vai.addSalirListener();
+
+        vmi.setVisible(true);
+    }
+
+    public void modificacionInmuebles(){
+        vai = new VentanaAltaInmuebles();
+
+        vai.addAceptarListener(new aceptarMouseListener());
+        vai.addSalirListener(new salirMouseListener());
+
+        vai.setVisible(true);
+    }
+
+    public class aceptarAltaMouseListener implements MouseListener{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
